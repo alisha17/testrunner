a86802b98644c84c9e156e9d5fc717b1405abe15
Alisha <anejaalisha@yahoo.com>
2017-06-30 05:19:39 +1000

    Cargo test crate

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,8 @@ use std::io::Read;
 use std::path::Path;
 use flate2::read::GzDecoder;
 use tar::Archive;
+use std::env;
+use std::process::{Command, Output};
 
 mod errors {
     // Create the Error, ErrorKind, ResultExt, and Result types
@@ -35,14 +37,14 @@ fn main() {
             writeln!(stderr, "caused by: {}", e).expect(errmsg);
         }
 
-        // The backtrace is not always generated. Try to run this example
-        // with `RUST_BACKTRACE=1`.
         if let Some(backtrace) = e.backtrace() {
             writeln!(stderr, "backtrace: {:?}", backtrace).expect(errmsg);
         }
 
         ::std::process::exit(1);
     }
+
+    let r = test_crate(&env::current_dir().unwrap(),"cargo", &"test");
 }
 
 fn download(url: &str) -> Result<reqwest::Response> {
@@ -56,7 +58,8 @@ fn unpack_to_folder<R: Read>(archive: &mut Archive<R>, path: &Path) -> Result<()
 }
 
 fn run() -> Result<()> {
-  let path = Path::new("/home/alisha/temp_crate");
+  let cwd = env::current_dir().unwrap();
+  let path = Path::new(&cwd);
   let url = "https://crates-io.s3-us-west-1.amazonaws.com/crates/zopfli/zopfli-0.3.3.crate";
   let bin = download(&url).chain_err(
         || format!("unable to download {}", url),
@@ -65,3 +68,30 @@ fn run() -> Result<()> {
   let r = unpack_to_folder(&mut tar, path).chain_err(|| "unable to unpack crate tarball");
   r
 }
+
+fn test_crate(cd: &Path, name: &str, args: &str) -> Result<()> {
+    let mut cmd = Command::new("cargo").arg("test").output()?;
+    let cmdstr = format!{"{:?}", cmd};
+
+    if cmd.status.success() {
+        Ok(())
+    } else {
+        Err(format!("command `{}` failed", cmdstr).into())
+    }
+}
+
+ /* .output().unwrap_or_else(|e| {
+            panic!("failed to execute process: {}", e)
+    });
+
+    if output.status.success() {
+        let s = String::from_utf8_lossy(&output.stdout);
+
+        print!("rustc succeeded and stdout was:\n{}", s);
+    } else {
+        let s = String::from_utf8_lossy(&output.stderr);
+
+        print!("rustc failed and stderr was:\n{}", s);
+    }
+}
+*/
