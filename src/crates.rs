f5dd152489e50fecef3584f83687112110a30a17
Alisha <anejaalisha@yahoo.com>
2017-07-14 22:37:17 +0530

    Trying to do generate_test!

--- src/crates.rs
+++ src/crates.rs
@@ -1,10 +1,21 @@
+extern crate reqwest;
+extern crate flate2;
+extern crate tar;
 extern crate toml;
+extern crate walkdir;
+
 use std::error::Error;
 use std::fs::File;
+use walkdir::WalkDir;
 use std::io::prelude::*;
-use std::path::Path;
+use std::path::{Path,PathBuf};
+use flate2::read::GzDecoder;
+use tar::Archive;
+use std::process::Command;
 use std::collections::HashMap;
 use toml::Value;
+
+use errors::*;
 
 pub fn get_crates_and_versions() -> HashMap<String, String> {
     let path_to_read = Path::new("Cargo-stdx.toml");
@@ -42,6 +53,39 @@ pub fn get_crates_and_versions() -> HashMap<String, String> {
         }
     }
     crate_info
+}
+
+pub fn download_tarballs(path: &PathBuf, crate_version: HashMap<String, String>) -> Result<()>{
+   for (crate_name, version) in crate_version {
+        let ver = format!("/{}-{}", &crate_name, version);
+        //let mut path_to_crate = path.join(ver);
+            //if Path::new(&path_to_crate).exists() == false {
+                let url = format!("https://crates-io.s3-us-west-1.amazonaws.com/crates/{0}/{0}-{1}.crate", crate_name, version);
+                let client = reqwest::Client::new().expect("could not setup https client");
+                let bin: reqwest::Response = client.get(&url).send()?;
+                let mut tar = Archive::new(GzDecoder::new(bin)?);
+                //println!("before unpacking to gitfolder");
+                let archive = &mut tar;
+                archive.unpack(&path).chain_err(|| "unable to unpack crate tarball")?;
+           // }
+            //test_crate(Path::new(&path_to_crate));
+        }     
+        Ok(())
+}
+
+// cargo test using std::process::Command
+
+pub fn test_crate(cache_dir: &PathBuf) -> Result<()> {
+    let cmd = Command::new("cargo").arg("test").current_dir(cache_dir).output()?;
+    let cmdstr = format!{"{:?}", cmd};
+    println!("{:?}", cmdstr);
+
+    if cmd.status.success() {
+        println!("It was a success!");
+        Ok(())
+    } else {
+        Err(format!("command `{}` failed", cmdstr).into())
+    }
 }
 
 
--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,7 @@ use flate2::read::GzDecoder;
 use tar::Archive;
 use std::process::Command;
 mod crates;
+mod runtest;
 
 mod errors {
     error_chain!{
@@ -33,47 +34,14 @@ mod errors {
 
 use errors::*;
 
-fn download_tarballs(path: &PathBuf, crate_version: HashMap<String, String>) -> Result<()>{
-   for (crate_name, version) in crate_version {
-        let ver = format!("/{}-{}", &crate_name, version);
-        let mut path_to_crate = path.join(ver);
-        println!("{:?}",path_to_crate);
-            //if Path::new(&path_to_crate).exists() == false {
-                let url = format!("https://crates-io.s3-us-west-1.amazonaws.com/crates/{0}/{0}-{1}.crate", crate_name, version);
-                let client = reqwest::Client::new().expect("could not setup https client");
-                let bin: reqwest::Response = client.get(&url).send()?;
-                let mut tar = Archive::new(GzDecoder::new(bin)?);
-                println!("before unpacking to folder");
-                let archive = &mut tar;
-                archive.unpack(&path).chain_err(|| "unable to unpack crate tarball")?;
-           // }
-            //test_crate(Path::new(&path_to_crate));
-        }     
-        Ok(())
-}
-
-// cargo test using std::process::Command
-
-fn test_crate(cd: &Path) -> Result<()> {
-    let cmd = Command::new("cargo").arg("test").current_dir(cd).output()?;
-    let cmdstr = format!{"{:?}", cmd};
-    println!("{:?}", cmdstr);
-
-    if cmd.status.success() {
-        println!("It was a success!");
-        Ok(())
-    } else {
-        Err(format!("command `{}` failed", cmdstr).into())
-    }
-}
-
 fn run() -> Result<()> {
   let cwd = "/home/alisha/temp_crate";
   let path = PathBuf::from(&cwd);
   let mut crate_version = crates::get_crates_and_versions();
-  download_tarballs(&path, crate_version);
+  crates::download_tarballs(&path, crate_version);
  // let cache_dir: PathBuf = env::var("OUT_DIR").join("tarball-cache")
  // println!("{:?}", crate_version);
+  //runtest::generate_tests(&path, crate_version);
   Ok(())
 }  
 
